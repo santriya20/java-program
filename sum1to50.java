006cf6885ee665a9c1f13a56af35463ed88b7169
santriya20 <tsantriya@gmail.com>
2025-07-31 18:00:52 +0530

    Create sum1to50.java

---
+++ sum1to50.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a=1;
+        int sum=0;
+        while(a<=50){
+        sum=sum+a;
+        a++;
+        }
+        System.out.println(sum);
+    }
+}
