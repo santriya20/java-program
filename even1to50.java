6ee7a3b5a84e9e3eeeca0041fdcb34d8af350c20
santriya20 <tsantriya@gmail.com>
2025-07-31 18:07:47 +0530

    Create even1to50.java

---
+++ even1to50.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int a=1;
+        while(a<=50){
+            if(a%2==0)
+        System.out.println(a);
+        a++;
+        }
+    }
+}
